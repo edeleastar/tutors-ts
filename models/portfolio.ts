85a61850ddbabb4f6b74047b8cee8f929a39c7e6
Eamonn de Leastar <edeleastar@wit.ie>
2017-06-06 17:10:32 +0100

    simplify portfolio reaping

--- models/portfolio.ts
+++ models/portfolio.ts
@@ -6,9 +6,14 @@ import * as fs from 'fs';
 import * as sh from 'shelljs';
 import * as yaml from 'yamljs';
 
-export class Portfolio extends CompositeLearningObject {
+interface CourseGroup {
+  title: string;
+  modules: string[];
+  courses: Array<Course>;
+}
 
-  yaml: any;
+export class Portfolio extends CompositeLearningObject {
+  courseGroups: Array<CourseGroup> = [];
 
   constructor(parent?: LearningObject) {
     super(parent);
@@ -17,14 +22,14 @@ export class Portfolio extends CompositeLearningObject {
   }
 
   reap(): void {
-    super.reap('portfolio');
-
-    this.yaml = yaml.load('./portfolio.yaml');
-    for (let courseGroup of this.yaml.courseGroups) {
-      courseGroup.courses = [];
-      for (let module of courseGroup.modules) {
+    const yamlData = yaml.load('./portfolio.yaml');
+    this.title = yamlData.title;
+    this.gitterid = yamlData.gitterid;
+    this.credits = yamlData.credits;
+    yamlData.courseGroups.forEach((courseGroup: CourseGroup) => {
+      courseGroup.courses = new Array<Course>();
+      courseGroup.modules.forEach((module: string) => {
         if (fs.existsSync(module)) {
-          // console.log('- ' + module);
           sh.cd(module);
           const course = new Course(this);
           if (course) {
@@ -34,16 +39,27 @@ export class Portfolio extends CompositeLearningObject {
         } else {
           console.log('- could not find ' + module);
         }
-      }
-    }
+      });
+      this.courseGroups.push(courseGroup);
+    });
   }
 
   publish(path: string): void {
     const absPath = getCurrentDirectory() + '/' + path;
 
-    publishTemplate(absPath, 'index.html', 'portfolio.html', this.yaml);
+    publishTemplate(absPath, 'index.html', 'portfolio.html', this);
 
-    for (let courseGroup of this.yaml.courseGroups) {
+    for (let courseGroup of this.courseGroups) {
+      for (let course of courseGroup.courses) {
+        const coursePath = absPath + '/' + course.folder;
+        verifyFolder(coursePath);
+        sh.cd(course.folder);
+        course.publish(coursePath);
+        sh.cd('..');
+      }
+    }
+
+    for (let courseGroup of this.courseGroups) {
       for (let course of courseGroup.courses) {
         const coursePath = absPath + '/' + course.folder;
         verifyFolder(coursePath);
