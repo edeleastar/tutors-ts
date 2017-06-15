cfe8e5769b341b9e3465467cebc251a14ee29ee6
Eamonn de Leastar <edeleastar@wit.ie>
2017-06-15 09:16:36 +0100

    portfolio to contain los directly

--- src/models/portfolio.ts
+++ src/models/portfolio.ts
@@ -1,5 +1,5 @@
 import {CompositeLearningObject, LearningObject} from './learningobjects';
-import {publishTemplate} from './loutils';
+import {publishLos, publishTemplate, reapLos} from './loutils';
 import {getCurrentDirectory, verifyFolder} from '../utils/futils';
 import {Course} from './course';
 import * as fs from 'fs';
@@ -28,24 +28,27 @@ export class Portfolio extends CompositeLearningObject {
 
   reap(): void {
     const yamlData = yaml.load('./portfolio.yaml');
+    super.los = reapLos(this);
     this.title = yamlData.title;
     this.subtitle = yamlData.subtitle;
     this.gitterid = yamlData.gitterid;
     this.credits = yamlData.credits;
     yamlData.courseGroups.forEach((courseGroup: CourseGroup) => {
       courseGroup.courses = new Array<Course>();
-      courseGroup.modules.forEach((module: string) => {
-        if (fs.existsSync(module)) {
-          sh.cd(module);
-          const course = new Course(this.options, this);
-          if (course) {
-            courseGroup.courses.push(course);
+      if (courseGroup.modules) {
+        courseGroup.modules.forEach((module: string) => {
+          if (fs.existsSync(module)) {
+            sh.cd(module);
+            const course = new Course(this.options, this);
+            if (course) {
+              courseGroup.courses.push(course);
+            }
+            sh.cd('..');
+          } else {
+            console.log('- could not find ' + module);
           }
-          sh.cd('..');
-        } else {
-          console.log('- could not find ' + module);
-        }
-      });
+        });
+      }
       this.courseGroups.push(courseGroup);
     });
   }
@@ -54,6 +57,7 @@ export class Portfolio extends CompositeLearningObject {
     const absPath = getCurrentDirectory() + '/' + path;
 
     publishTemplate(absPath, 'index.html', 'portfolio.html', this);
+    publishLos(path, this.los);
 
     for (let courseGroup of this.courseGroups) {
       for (let course of courseGroup.courses) {
