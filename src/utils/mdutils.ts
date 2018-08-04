a4e8c9241df62b7e64738f88f2e54d5c8d13ab2d
Eamonn de Leastar <edeleastar@wit.ie>
2018-08-04 10:01:53 +0200

    more prettier style updates

--- src/utils/mdutils.ts
+++ src/utils/mdutils.ts
@@ -1,10 +1,10 @@
 import * as fs from 'fs';
-import {readFile} from './futils';
+import { readFile } from './futils';
 
 const marked = require('../utilsjs/marked');
 
 export function parse(fileName: string): string {
-  let mdContent = "";
+  let mdContent = '';
   if (fs.existsSync(fileName)) {
     mdContent = fs.readFileSync(fileName).toString();
   }
@@ -25,7 +25,10 @@ export function parseWithoutHeader(fileName: string): string {
 
 export function getHeader(fileName: string): string {
   let header = '';
-  let array = fs.readFileSync(fileName).toString().split('\n');
+  let array = fs
+    .readFileSync(fileName)
+    .toString()
+    .split('\n');
   if (array[0][0] === '#') {
     header = array[0].substring(1);
   } else {
@@ -49,12 +52,11 @@ export function getHeader(fileName: string): string {
 //   }
 // }
 
-const replicate = function(len:number, c:string) {
-  return Array(len+1).join(c || ' ');
-}
+const replicate = function(len: number, c: string) {
+  return Array(len + 1).join(c || ' ');
+};
 
-export function padRight(text:string, len:number, char:string) {
+export function padRight(text: string, len: number, char: string) {
   if (text.length >= len) return text;
-  return text + replicate(len-text.length, char);
+  return text + replicate(len - text.length, char);
 }
-
